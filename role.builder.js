10ef3154ecd3599a6619b10e28a2cc9064ba2f02
Dogu <dogudeniz.ugur@gmail.com>
2020-08-05 00:12:16 +0300

    a

--- role.builder.js
+++ role.builder.js
@@ -1,40 +1,39 @@
 var roleBuilder = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
         // Building, out of energy
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+        if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
             creep.say('🔄 harvest');
         }
         // Not builiding, full
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+        if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
             creep.memory.building = true;
             creep.say('🚧 build');
         }
         // Building
-        if(creep.memory.building) {
+        if (creep.memory.building) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             targets = _.sortBy(targets, s => creep.pos.getRangeTo(s))
-            if(targets.length > 0) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            if (targets.length > 0) {
+                if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#0000ff'}});
                 }
             }
             // If no targets turn into harvester
-            else {
-                console.log('Builder turned to harvester.')
-                creep.memory.role = 'harvester'
-            }
-        }
-        else if(creep.store.getFreeCapacity() > 0) {
+            /*
+        else {
+            console.log('Builder turned to harvester.')
+            creep.memory.role = 'harvester'
+        }*/
+        } else if (creep.store.getFreeCapacity() > 0) {
             let sources = creep.room.find(FIND_SOURCES_ACTIVE);
             //sources = _.sortBy(sources, s => creep.pos.getRangeTo(s))
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#0000ff'}});
             }
-        }
-        else {
+        } else {
             console.log('Builder turned to harvester.')
             creep.memory.role = 'harvester'
         }
