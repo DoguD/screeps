d9c5cde1087a7286d16cdb93de32905cd610f4b6
Dogu <dogudeniz.ugur@gmail.com>
2020-08-05 00:46:23 +0300

    a

--- spawner.js
+++ spawner.js
@@ -1,15 +1,15 @@
 let spawn = Game.spawns['Spawn1'];
 let population = {
-        harvester: 2,
-        upgrader: 2,
-        builder: 2
+    harvester: spawn.memory.pHarvester,
+    builder: spawn.memory.pBuilder,
+    upgrader: spawn.memory.pUpgrader,
 };
 
 var spawner = {
     checkSpawn: function () {
         // HARVESTERS
         let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
-        if(harvesters.length < population.harvester) {
+        if (harvesters.length < population.harvester) {
             let newName = 'Harvester' + Game.time;
             spawn.spawnCreep([WORK, WORK, CARRY, MOVE], newName,
                 {memory: {role: 'harvester'}});
@@ -17,16 +17,16 @@ var spawner = {
 
         // UPGRADERS
         let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
-        if(upgraders.length < population.upgrader) {
+        if (upgraders.length < population.upgrader) {
             let newName = 'Upgrader' + Game.time;
             spawn.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'upgrader'}});
         }
 
         // BUILDER
         let builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
-        if(builders.length < population.builder) {
+        if (builders.length < population.builder) {
             let newName = 'Builder' + Game.time;
-            spawn.spawnCreep([WORK, WORK, CARRY,MOVE], newName, {memory: {role: 'builder'}});
+            spawn.spawnCreep([WORK, WORK, CARRY, MOVE], newName, {memory: {role: 'builder'}});
         }
         //console.log('Harvesters:' + harvesters.length.toString() + ' / Upgraders: ' + upgraders.length.toString() + ' / Builders: ' + builders.length.toString())
     }
