8da18e81c065fb281e837ee15afc12d1b18c43a5
Dogu <dogudeniz.ugur@gmail.com>
2020-08-05 00:21:41 +0300

    a

--- role.harvester.js
+++ role.harvester.js
@@ -20,6 +20,9 @@ var roleHarvester = {
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
+            if (creep.name === 'Harvester20391420') {
+                console.log('hey');
+            }
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#00ff00'}});
