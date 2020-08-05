0d855a5370a3687d83e943c5d1e9fb1ecd6b0886
Dogu <dogudeniz.ugur@gmail.com>
2020-08-05 14:18:18 +0300

    a

--- sourcefinder.js
+++ sourcefinder.js
@@ -4,10 +4,13 @@ let sourceFinder = {
         let activeSources = room.find(FIND_SOURCES_ACTIVE);
         let availableSources = [];
         activeSources.forEach((item, index) => {
+            let available = false;
             let sourceLocation = item.pos;
-            let topLeft = room.lookAt(sourceLocation.x, sourceLocation.y);
+            let topLeft = room.lookAt(sourceLocation.x+1, sourceLocation.y+1);
             topLeft.forEach((item, index) => {
-                console.log(item.type);
+                if (item.type === 'terrain') {
+                    console.log(item.terrain);
+                }
             })
         })
     }
