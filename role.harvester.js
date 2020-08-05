f2ed14d9f2ace2a57e2838c750abf57b50171827
Doğu Deniz Uğur <dogudeniz.ugur@gmail.com>
2020-08-05 15:13:21 +0300

    Harvesters harvest and drop other creeps pickup and do work (#1)
    * Harvesters only harvest and drop no movement
    
    * Spawner body parts changed
    
    * Builder and upgrader adjusted to picking up

--- role.harvester.js
+++ role.harvester.js
@@ -1,37 +1,33 @@
-let roles = {
-    builder: require('role.builder'),
-};
 let sourceFinder = require('sourcefinder');
 
 var roleHarvester = {
     run: function (creep) {
-        sourceFinder.findAvailableSource(creep);
         // Check if there is free capacity
         if (creep.store.getFreeCapacity() > 0) {
-            // Get closest source with energy
-            let sources = creep.room.find(FIND_SOURCES_ACTIVE);
-            //sources = _.sortBy(sources, s => creep.pos.getRangeTo(s))
-            // Harvest or move
-            if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#00ff00'}});
-            }
-        } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            // Check if there is any source adjacent
+            let targetSource = null;
+            let sources = creep.room.find(FIND_SOURCES);
+            sources.forEach((item, index) => {
+                if (Math.abs(item.pos.x - creep.pos.x) < 2 && Math.abs(item.pos.y - creep.pos.y) < 2) {
+                    targetSource = item;
                 }
             });
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#00ff00'}});
+            if (targetSource) {
+                if (creep.harvest(targetSource) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targetSource, {visualizePathStyle: {stroke: '#00ff00'}});
                 }
             }
-            // If all stocks are full do builder job
+            // If no adjacent search for available
             else {
-                roles.builder.run(creep);
-                //console.log(creep.name + ' doing build.');
+                let availableSources = sourceFinder.findAvailableSource(creep);
+                if (availableSources.length > 0) {
+                    creep.moveTo(availableSources[0], {visualizePathStyle: {stroke: '#00ff00'}});
+                }
             }
+        }
+        // Drop when full energy
+        else {
+            creep.drop(RESOURCE_ENERGY);
         }
     }
 };
